19efc9f59be27a27a32a62b2e0d3acb31e824386
saltas888 <d.saltaferis@gmail.com>
2020-12-14 16:26:17 +0100

    Remove comment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dealroom-urlextract",  # Replace with your own username
+    name="dealroom-urlextract",
     version="0.0.1",
     author="Dealroom.co",
     author_email="data@dealroom.co",
