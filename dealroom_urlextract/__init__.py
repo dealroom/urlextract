f3aac5b1665e5e4e6b232d9cda82aa5c19480c09
GuillermoDR <70137647+GuillermoDR@users.noreply.github.com>
2022-04-13 11:29:57 +0200

    Remove comma from invalid characters list

--- dealroom_urlextract/__init__.py
+++ dealroom_urlextract/__init__.py
@@ -3,7 +3,7 @@ import tldextract
 class InvalidURLFormat(Exception):
     pass
 
-INVALID_CHARACTERS = [" ", "\\", "<", ">", "{", "}", ";", ","]
+INVALID_CHARACTERS = [" ", "\\", "<", ">", "{", "}", ";"]
 
 def extract(url: str, keep_subdomain:bool = True):
     """Extract the correct formatting for a passed url.
