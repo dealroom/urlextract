f44320d811eb89aacc2cead5b52ddb33f49fdece
Matteo Renoldi <matteo@renoldi.org>
2023-06-14 14:47:44 +0200

    added some more tests

--- tests/test_dealroom_urlextract.py
+++ tests/test_dealroom_urlextract.py
@@ -23,3 +23,31 @@ def test_format(unformatted_name_surname) -> None:
     ) = unformatted_name_surname
     with expectation:
         assert extract(url=original_url, keep_subdomain=True) is not None
+
+
+test_data_raw_website_urls = [
+    ("http://www.something.com/home.html?abc", "something.com"),
+    ("https://burgernation.us", "burgernation.us"),
+    ("http://haribo.com/", "haribo.com"),
+    ("www.balderton.com", "balderton.com"),
+    ("www.thetradedesk.com", "thetradedesk.com"),
+    ("http://www.railsbank.com/", "railsbank.com"),
+    ("https://www.ecb.eu/", "ecb.eu"),
+]
+
+
+@pytest.mark.parametrize(
+    "url, expected",
+    test_data_raw_website_urls,
+    ids=[
+        "something",
+        "burgernation",
+        "haribo",
+        "balderton",
+        "thetradedesk",
+        "railsbank",
+        "ecb",
+    ],
+)
+def test_url_extract(url, expected) -> None:
+    assert extract(url) == expected
